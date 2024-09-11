cb5c55626f6b8f7211ab116cb4329606a61732d4
Siddhant <siddhantg314@gmail.com>
2024-09-11 13:51:24 -0400

    solved vercel build issues v23

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -45,17 +45,19 @@ const PaginationLink = ({
   size = "icon",
   ...props
 }: PaginationLinkProps) => (
-  <a
-    aria-current={isActive ? "page" : undefined}
-    className={cn(
-      buttonVariants({
-        variant: isActive ? "outline" : "ghost",
-        size,
-      }),
-      className
-    )}
-    {...props}
-  />
+  <PaginationItem>
+    <a
+      aria-current={isActive ? "page" : undefined}
+      className={cn(
+        buttonVariants({
+          variant: isActive ? "outline" : "ghost",
+          size,
+        }),
+        className
+      )}
+      {...props}
+    />
+  </PaginationItem>
 )
 PaginationLink.displayName = "PaginationLink"
 
